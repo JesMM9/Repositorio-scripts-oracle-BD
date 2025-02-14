125eb132e18fb95c040b147fe1c3daa195dbcf2c
JesMM9 <jes9mimi9@gmail.com>
2025-02-14 20:07:58 +0100

    Comenzado el ejercicio de refuerzo y outerjoin

---
+++ Ejercicio de refuerzo y outer join 1.sql
@@ -0,0 +1,43 @@
+DROP TABLE DEPARTAMENTO;
+DROP TABLE EMPLEADO;
+
+CREATE TABLE DEPARTAMENTO
+(
+	CODIGO NUMBER(10),
+	NOMBRE VARCHAR2(100),
+	PRESUPUESTO DECIMAL(6,2),
+	CONSTRAINT PK_DEPARTAMENTO PRIMARY KEY (CODIGO)
+);
+
+CREATE TABLE EMPLEADO
+(
+	CODIGO NUMBER(10),
+	NIF VARCHAR2(9),
+	NOMBRE VARCHAR2(100),
+	APELLIDO1 VARCHAR2(100),
+	APELLIDO2 VARCHAR2(100),
+	CODIGO_DEPARTAMENTO NUMBER(10),
+	CONSTRAINT PK_EMPLEADO PRIMARY KEY (CODIGO),
+	CONSTRAINT FK1_EMPLEADO FOREIGN KEY (CODIGO_DEPARTAMENTO) REFERENCES DEPARTAMENTO(CODIGO)
+);
+
+INSERT INTO departamento VALUES(2, 'Sistemas', 150000, 21000);
+INSERT INTO departamento VALUES(3, 'Recursos Humanos', 280000, 25000);
+INSERT INTO departamento VALUES(4, 'Contabilidad', 110000, 3000);
+INSERT INTO departamento VALUES(5, 'I+D', 375000, 380000);
+INSERT INTO departamento VALUES(6, 'Proyectos', 0, 0);
+INSERT INTO departamento VALUES(7, 'Publicidad', 0, 1000);
+INSERT INTO empleado VALUES(1, '32481596F', 'Aarón', 'Rivero', 'Gómez', 1);
+INSERT INTO empleado VALUES(2, 'Y5575632D', 'Adela', 'Salas', 'Díaz', 2);
+INSERT INTO empleado VALUES(3, 'R6970642B', 'Adolfo', 'Rubio', 'Flores', 3);
+INSERT INTO empleado VALUES(4, '77705545E', 'Adrián', 'Suárez', NULL, 4);
+INSERT INTO empleado VALUES(5, '17087203C', 'Marcos', 'Loyola', 'Méndez', 5);
+INSERT INTO empleado VALUES(6, '38382980M', 'María', 'Santana', 'Moreno', 1);
+INSERT INTO empleado VALUES(7, '80576669X', 'Pilar', 'Ruiz', NULL, 2);
+INSERT INTO empleado VALUES(8, '71651431Z', 'Pepe', 'Ruiz', 'Santana', 3);
+INSERT INTO empleado VALUES(9, '56399183D', 'Juan', 'Gómez', 'López', 2);
+INSERT INTO empleado VALUES(10, '46384486H', 'Diego','Flores', 'Salas', 5);
+INSERT INTO empleado VALUES(11, '67389283A', 'Marta','Herrera', 'Gil', 1);
+INSERT INTO empleado VALUES(12, '41234836R', 'Irene','Salas', 'Flores', NULL);
+INSERT INTO empleado VALUES(13, '82635162B', 'Juan Antonio','Sáez', 'Guerrero',NULL);
+
