4313540d327db4be71a00353f1d3350424ba0fc1
JesMM9 <jes9mimi9@gmail.com>
2025-03-01 19:13:34 +0100

    Terminado el boletín group by de gestión

--- Group by del boletín de gestión.sql
+++ Group by del boletín de gestión.sql
@@ -55,6 +55,103 @@ SELECT MIN(FECHA), MAX(FECHA)
 FROM FACTURAS F
 WHERE CODCLI=210;
 
+--11. Número de artículos cuyo stock es nulo.
+
+SELECT COUNT(CODART)
+FROM ARTICULOS
+WHERE STOCK IS NULL;
+
+--12. Número de líneas cuyo descuento es nulo (con un decimal)
+
+SELECT COUNT(LINEA)
+FROM LINEAS_FAC
+WHERE DTO IS NULL;
+
+--13. Obtener cuántas facturas tiene cada cliente.
+
+SELECT COUNT(F.CODFAC)
+FROM FACTURAS F JOIN CLIENTE C
+ON F.CODCLI=C.CODCLI;
+
+--14. Obtener cuántas facturas tiene cada cliente, pero sólo si tiene dos o más  facturas.
+
+SELECT COUNT(F.CODFAC)
+FROM FACTURAS F JOIN CLIENTE C
+ON F.CODCLI=C.CODCLI
+HAVING COUNT(F.CODFAC)>=2;
+
+--15. Importe de la facturación (suma del producto de la cantidad por el precio de las líneas de factura) de los  artículos.
+
+SELECT SUM(CANT*PRECIO)
+FROM LINEAS_FAC;
+
+--16. Importe de la facturación (suma del producto de la cantidad por el precio de las líneas de factura) de aquellos artículos cuyo código contiene la letra “A” (bien mayúscula o minúscula).
+
+SELECT SUM(CANT*PRECIO)
+FROM LINEAS_FAC LF JOIN FACTURAS F
+ON LF.CODFAC=F.CODFAC
+WHERE UPPER(F.CODCLI) LIKE '%A%';
+
+--17. Número de facturas para cada fecha, junto con la fecha.
+
+SELECT CODFAC, FECHA
+FROM FACTURAS
+GROUP BY FECHA;
+
+--18. Obtener el número de clientes del pueblo junto con el nombre del pueblo mostrando primero los pueblos que más clientes tengan.
+
+SELECT COUNT(CODCLI), NOMBRE
+FROM PUEBLOS
+ORDER BY COUNT(CODCLI);
+
+--19. Obtener el número de clientes del pueblo junto con el nombre del pueblo mostrando primero los pueblos que más clientes tengan, siempre y cuando tengan más de dos clientes.
+
+SELECT COUNT(CODCLI), NOMBRE
+FROM PUEBLOS
+HAVING COUNT(CODCLI)>=2
+ORDER BY COUNT(CODCLI);
+
+--20. Cantidades totales vendidas para cada artículo cuyo código empieza por “P", mostrando también la descripción de dicho artículo.
+
+SELECT LF.CANT, A.DESCRIP
+FROM LINEAS_FAC LF JOIN ARTICULOS A
+ON LF.CODART=A.CODART
+WHERE UPPER(A.CODART) LIKE 'P%';
+
+--21. Precio máximo y precio mínimo de venta (en líneas de facturas) para cada artículo cuyo código empieza por “c”.
+
+SELECT MAX(PRECIO), MIN(PRECIO)
+FROM LINEAS_FAC
+WHERE UPPER(CODART) LIKE 'C%';
+
+--22. Igual que el anterior pero mostrando también la diferencia entre el precio máximo y mínimo.
+
+SELECT MAX(PRECIO), MIN(PRECIO), MAX(PRECIO)-MIN(PRECIO) DIFERENCIA
+FROM LINEAS_FAC
+WHERE UPPER(CODART) LIKE 'C%';
+
+--23. Nombre de aquellos artículos de los que se ha facturado más de 10000 euros.
+
+SELECT A.NOMBRE
+FROM ARTICULOS A JOIN LINEAS_FAC LF
+ON A.CODART=LF.CODART
+WHERE LF.PRECIO>10000;
+
+--24. Número de facturas de cada uno de los clientes cuyo código está entre 150 y 300 (se debe mostrar este código), con cada IVA distinto que se les ha aplicado.
+
+SELECT DISTINCT IVA, CODFAC, CODCLI
+FROM FACTURAS
+WHERE CODCLI BETWEEN 150 AND 300;
+
+--25. Media del importe de las facturas, sin tener en cuenta impuestos ni descuentos.
+
+SELECT AVG(LF.PRECIO-F.IVA+F.DTO)
+FROM FACTURAS F JOIN LINEAS_FAC LF
+ON F.CODFAC=LF.CODFAC;
+
+
+
+
 
 
 
